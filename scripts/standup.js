ebaf714eac92b592b003cab684154234a7828588
brandenbyers <branden@brandenbyers.com>
2015-06-17 17:03:23 -0500

    Add space before Eastern

--- scripts/standup.js
+++ scripts/standup.js
@@ -178,7 +178,7 @@ module.exports = function(robot) {
         var room = findRoom(msg);
 
         saveStandup(room, time);
-        msg.send("Ok, from now on I'll remind this room to do a standup every weekday at " + time + "Eastern Time.");
+        msg.send("Ok, from now on I'll remind this room to do a standup every weekday at " + time + " Eastern Time.");
     });
 
     robot.respond(/create standup ((?:[01]?[0-9]|2[0-4]):[0-5]?[0-9]) UTC([+-][0-9])$/i, function(msg) {
@@ -195,7 +195,7 @@ module.exports = function(robot) {
         var standups = getStandupsForRoom(findRoom(msg));
 
         if (standups.length === 0) {
-            msg.send("Well this is awkward. You haven't got any standups set :-/");
+            msg.send("Well this is awkward. You don't have any standups set :-/");
         }
         else {
             var standupsText = ["Here are your standups:"].concat(_.map(standups, function (standup) {
