6fe01fb3bf18bc89488461fcbfa4dccd059f8a80
brandenbyers <branden@brandenbyers.com>
2015-06-26 12:01:21 -0500

    Add more responses

--- scripts/standup.js
+++ scripts/standup.js
@@ -40,7 +40,26 @@ module.exports = function(robot) {
         "Of course you can have another cupcake! But first; it's standup time.\nhttps://i.imgflip.com/n1qyk.jpg",
         "Slides are for suckers! Just ride on my gut to standup!\nhttps://i.imgflip.com/n1r4y.jpg",
         "Standup time\nC'mon, grab your co-workers\nWe'll go to very distant lands\nWith Hubot the bot\nAnd you weird humans\nThe standups'll never end\nIt's Standup Time!\nhttp://i.imgflip.com/n2uf8.gif",
-        "Standup.\nhttps://i.imgflip.com/n2vj1.jpg"
+        "Standup.\nhttps://i.imgflip.com/n2vj1.jpg",
+        "Fly high with your luck dragon on this mighty quest called stand up.\nhttp://i.imgflip.com/nfxar.gif",
+        "Call my name. Bastian, please! Save us!\nFantasia can arise anew, from your dreams and wishes in Standup today.\nhttp://media.giphy.com/media/RIRGZT00VynzG/giphy.gif",
+        "Standup-aq Attaq. It's time to Standup tall.\nhttp://media.giphy.com/media/oFP9WKihsBjMs/giphy.gif",
+        "The pleasure of Standup is here. Rejoice.\nhttp://media.giphy.com/media/yidUzkciDTniZ7OHte/giphy.gif",
+        "Surfs up for Standup\nhttp://media.giphy.com/media/e89Of8NDEuFXy/giphy.gif",
+        "Stop clowning around...\nIt is time for Standup\nhttp://media.giphy.com/media/vOI59yRWCg9Jm/giphy.gif",
+        "Standup time is back!\nhttp://media.giphy.com/media/U6FxcaHoKdD0I/giphy.gif",
+        "Oops! It's Standup again. Oh baby, baby.\nhttp://media.giphy.com/media/AchMSP0WqKKo8/giphy.gif\nYeah yeah yeah yeah yeah yeah",
+        "Hey, do any of you guys or gals know where Standup is at today?",
+        "Hurry, it's time for Standup. Don't be late\nhttp://media.giphy.com/media/DoAz2vNvAF3pe/giphy.gif",
+        "Pika Pika Pika-STANDUP-chu!\nhttp://media.giphy.com/media/ydUqexuZ4QTWo/giphy.gif",
+        "Wake up. It's Standup.\nhttp://media.giphy.com/media/TgGkZOh7hUpVu/giphy.gif",
+        "Watch out...it's Standup time.\nhttp://media.giphy.com/media/pXPOApWXuJaMw/giphy.gif",
+        "It's mind-blowing Standup time.\nhttp://media.giphy.com/media/KUb97RUwAsuf6/giphy.gif",
+        "You'll have to hold it until after Standup.\nhttp://media.giphy.com/media/q22xX098bWU/giphy.gif",
+        "Standup party time.\nhttp://giphy.com/gifs/taco-transparent-fun-3KAxGKtEQpTmo",
+        "The power of voodoo.\nWho do?\nYou do.\nDo what?\nSTANDUP\nhttp://media.giphy.com/media/Wer0THzNFUoYE/giphy.gif",
+        "Hold on to your heads, it is Standup time.\nhttp://media.giphy.com/media/10Kt9EtOLQSXPa/giphy.gif",
+        "No complaining. It's time for Standup.\nhttp://media.giphy.com/media/N72vsMfkYbHry/giphy.gif"
     ];
 
     var PREPEND_MESSAGE = process.env.HUBOT_STANDUP_PREPEND || "";
